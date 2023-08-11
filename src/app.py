b621f90569ba194f8696be1386f7d35de3f0c947
Alexis Sayago <as.sayagom@gmail.com>
2023-08-11 17:11:31 -0400

    equipment delete and put methods adedd

--- src/app.py
+++ src/app.py
@@ -71,6 +71,40 @@ def handle_equipment():
             "msg": "equipment added"
         }), 200
 
+@app.route('/equipment/<int:position>', methods=['PUT', "DELETE"])
+def update_equipment(id):
+    if request.method == 'DELETE':
+        equipment = Equipment.query.get(id)
+        if equipment is not None:
+            db.session.delete(equipment)
+            db.session.commit()
+
+            return jsonify({
+                "msg": "equipment deleted"
+            }), 202
+        else:
+            return jsonify({
+                "msg": "equipment not found"
+            }), 404
+    elif request.method == 'PUT':
+        equipment = Equipment.query.get(id)
+        if equipment is not None:
+            data = request.get_json()
+            equipment.name = data["name"]
+            equipment.description = data["description"]
+            equipment.status = data["status"]
+            equipment.is_active = data["is_active"]
+
+            db.session.commit()
+
+            return jsonify({
+                "msg": "equipment updated"
+            }), 200
+        else:
+            return jsonify({
+                "msg": "equipment not found"
+            }), 404
+
 
 #no modificar desde este punto hacia abajo
 
